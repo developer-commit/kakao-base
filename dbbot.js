1d7191b7e60f3d2b452026c7c45ccb8597275558
developer-commit <yje72826045@gmail.com>
2024-10-14 01:27:12 +0900

    Update dbbot.js

--- dbbot.js
+++ dbbot.js
@@ -10,6 +10,8 @@ var main = require('main');
  * (string) imageDB.getProfileBase64()
  * (string) packageName
  */
+
+//대응소스는 자체적으로 구해서 추가하세요
 function responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, user) {
   try{
 
@@ -43,34 +45,3 @@ function onResume(activity) {}
 function onPause(activity) {}
 
 function onStop(activity) {}
-
-function onNotificationPosted(sbn, sm) {
-  var packageName = sbn.getPackageName();
-  if (!packageName.startsWith("com.kakao.tal")) return;
-  var actions = sbn.getNotification().actions;
-  if (actions == null) return;
-  var userId = sbn.getUser().hashCode();
-
-  for (var n = 0; n < actions.length; n++) {
-      var action = actions[n];
-      if (action.getRemoteInputs() == null) continue;
-      var bundle = sbn.getNotification().extras;
-      var user = bundle.get("android.messages")[0].get("sender_person").key;
-
-      var msg = bundle.get("android.text").toString();
-      var sender = bundle.getString("android.title");
-      var room = bundle.getString("android.subText");
-      if (room == null) room = bundle.getString("android.summaryText");
-      var isGroupChat = room != null;
-      if (room == null) room = sender;
-      var replier = new com.xfl.msgbot.script.api.legacy.SessionCacheReplier(packageName, action, room, false, "");
-      var icon = bundle.getParcelableArray("android.messages")[0].get("sender_person").getIcon().getBitmap();
-      var image = bundle.getBundle("android.wearable.EXTENSIONS");
-      if (image != null) image = image.getParcelable("background");
-      var imageDB = new com.xfl.msgbot.script.api.legacy.ImageDB(icon, image);
-      com.xfl.msgbot.application.service.NotificationListener.Companion.setSession(packageName, room, action);
-      if (this.hasOwnProperty("responseFix")) {
-          responseFix(room, msg, sender, isGroupChat, replier, imageDB, packageName, user);
-      }
-  }
-}
